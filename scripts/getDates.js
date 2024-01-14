7ef63f735dc9d5ae8e1755c59eb124665b4adc4c
divineProjects <divinejigu@gmail.com>
2024-01-14 03:55:54 +0200

    w01

---
+++ scripts/getDates.js
@@ -0,0 +1,7 @@
+let oLastModif = new Date(document.lastModified);
+todayDate = new Date();
+document.querySelector('#currentYear').textContent = todayDate.getFullYear();
+document.querySelector('#lastModified').textContent = `Last Modified: ${oLastModif}`;
+
+
+
