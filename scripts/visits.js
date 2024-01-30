6a206a4c9dcd06f1fab34104b743701c803349e5
divineProjects <divinejigu@gmail.com>
2024-01-30 16:27:24 +0200

    Learning Activities update

---
+++ scripts/visits.js
@@ -0,0 +1,13 @@
+const displayNumOfVisits = document.querySelector(".visits");
+//Get the stored VALUE in localStorage if it exists. 
+let numOfVisits = Number(window.localStorage.getItem("numOfVisits-ls")) || 0;
+
+if (numOfVisits !== 0) {
+    displayNumOfVisits.textContent = numOfVisits;
+} else {
+    visitsDisplay.textContent = `🥳 Welcome!!! This is your first visit.`;
+}
+
+numOfVisits++;
+localStorage.setItem("numOfVisits-ls", numOfVisits);
+
