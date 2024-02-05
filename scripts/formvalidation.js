263cf543138fc70bdc6f2e078b140d41b5fbffe3
divineProjects <divinejigu@gmail.com>
2024-02-05 15:11:56 +0200

    password updated

--- scripts/formvalidation.js
+++ scripts/formvalidation.js
@@ -2,5 +2,19 @@ const value = document.querySelector("#value");
 const input = document.querySelector("#pi_input");
 value.textContent = input.value;
 input.addEventListener("input", (event) => {
-  value.textContent = event.target.value;
+    value.textContent = event.target.value;
 });
+
+const pwd = document.querySelector('#pwd');
+const pwd2 = document.querySelector('#pwd2');
+const pwderror = document.querySelector('.pwderror');
+
+
+pwd2.addEventListener('input', () => {
+    if (pwd.value !== pwd2.value) {
+        pwderror.style.display = 'block';
+    } else {
+        pwderror.style.display = 'none';
+    }
+});
+
