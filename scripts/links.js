619a57de29e71e3617b1ab31222a237f0a0367f2
divineProjects <divinejigu@gmail.com>
2024-02-12 17:05:25 +0200

    update

--- scripts/links.js
+++ scripts/links.js
@@ -1,3 +1,40 @@
+// select HTML elements in the document
+const weather = document.querySelector('.weather');
+// const url = `//api.openweathermap.org/data/2.5/onecall?lat={49.750265627063186}&lon={6.6315950909878545}&appid={57b35b35ea4398840f483c29f14b8533}';
+
+key = '57b35b35ea4398840f483c29f14b8533';
+lattitude = '-20.1525730567213';
+longitude = '28.58308545902036';
+const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lattitude}&lon=${longitude}&appid=${key}`;
+
+
+async function apiFetch() {
+
+    try {
+        const response = await fetch(url);
+        if (response.ok) {
+            const data = await response.json();
+            // console.log(data);
+            displayResults(data); // uncomment when ready
+        } else {
+            throw Error(await response.text());
+        }
+    } catch (error) {
+        console.log(error);
+    };
+};
+
+function displayResults(data) {
+    const iconsrc = `https://openweathermap.org/img/w/${data.weather[0].icon}.png`;
+    weather.innerHTML = `<img src="${iconsrc}" alt="${data.weather[0].description}" id="imgWeather"> ${data.main.temp}&deg;K - ${data.weather[0].description}`;
+}
+
+apiFetch();
+
+
+
+
+
 
 
 
