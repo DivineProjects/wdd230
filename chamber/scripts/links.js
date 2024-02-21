8ba3a2e2eb180709f71c54dc5baacb1f89272554
divineProjects <divinejigu@gmail.com>
2024-02-21 08:03:59 +0200

    update js chambers

--- chamber/scripts/links.js
+++ chamber/scripts/links.js
@@ -38,7 +38,7 @@ function displayResults(data) {
                             </div>
                             <div class="temp">
                                 <div class="infoPart topInfo">
-                                    <div class="temper"> ${Math.round(data.main.temp)}</div>
+                                    <div class="temper"> ${Math.round(data.main.temp)}&deg;C</div>
                                 </div>
                                 <div class="infoPart topInfo">
                                     <div class="discWeather">${data.weather[0].description}</div>
@@ -52,47 +52,6 @@ function displayResults(data) {
 }
 
 apiFetch();
-
-// Dynamic Activity Links
-
-const baseURL = "https://divineprojects.github.io/wdd230/";
-const linksURL = "https://divineprojects.github.io/wdd230/data/links.json"
-const card = document.querySelector('#card1');
-
-async function getLinks() {
-    const response = await fetch(linksURL);
-    const data = await response.json();
-    // console.log(data.weeks);
-    displayLinks(data.weeks);
-
-}
-
-function displayLinks(weeks) {
-
-    weeks.forEach(week => {
-        let ul = document.createElement("ul");
-        let li = document.createElement("li");
-        let i = 1;
-        let ancher = "";
-        // loop & convert lists into anchers
-        week.links.forEach(link => {
-            if (i === 1) {
-                ancher = ancher + `${week.week}: <a href="${link.url}" target="_blank">${link.title}</a>`;
-            } else {
-                ancher = ancher + ` | <a href="${link.url}" target="_blank">${link.title}</a>`
-            }
-
-            i += 1;
-        });
-
-        li.innerHTML = ancher;
-        ul.appendChild(li);
-        card.appendChild(ul);
-
-    });
-}
-
-getLinks();
 
 
 const apiKey = 'YOUR_API_KEY';
